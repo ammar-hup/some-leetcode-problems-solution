d84b2c96525a0736901bda1b19125da0cd611b92
Ammar Mahmoud <69160488+ammar-hup@users.noreply.github.com>
2023-03-06 17:28:02 +0200

    Create calculate the digits.cpp

---
+++ calculate the digits.cpp
@@ -0,0 +1,31 @@
+vector<int> solution(vector<int> a) {
+    vector<int> b;
+    
+    for(int i = 0; i < a.size(); i++){
+        while(a[i] != 0){
+            b.push_back(a[i] % 10);
+            a[i] /= 10;
+        }
+    }
+    
+    int freq[1001] = {0};
+    
+    for(auto o : b) freq[o]++;
+    
+    int mx = *max_element(freq, freq + 1001);
+    
+    set<int> s;
+    
+    for(auto o : b) {
+        if (freq[o] == mx){
+            s.insert(o);
+        }   
+    }
+    
+    vector<int> ans;
+    for(auto o : s) {
+        ans.push_back(o);
+    }
+    
+    return ans;
+}
