ace4313891c29b21315dbea0232b510c8aabf339
Ammar Mahmoud <69160488+ammar-hup@users.noreply.github.com>
2023-11-19 20:09:11 +0200

    994. Rotting Oranges.py

---
+++ Py/Code  Testcase Testcase Test Result  994. Rotting Oranges.py
@@ -0,0 +1,44 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        
+        rows = len(grid)
+        if rows == 0:
+            return -1
+        cols = len(grid[0])
+        fresh_cnt = 0
+        # queue with rotten oranges (for BFS)
+        rotten = deque()
+        # visit each cell in the grid
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    # add the rotten orange coordinates to the queue
+                    rotten.append((r, c))
+                elif grid[r][c] == 1:
+                    # update fresh oranges count
+                    fresh_cnt += 1
+        # keep track of minutes passed.
+        minutes_passed = 0
+        while rotten and fresh_cnt > 0:
+            minutes_passed += 1
+            # process rotten oranges on the current level
+            for _ in range(len(rotten)):
+                x, y = rotten.popleft()
+                
+                # visit all the adjacent cells
+                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    # calculate the coordinates of the adjacent cell
+                    xx, yy = x + dx, y + dy
+                    # ignore the cell if it is out of the grid boundary
+                    if xx < 0 or xx == rows or yy < 0 or yy == cols:
+                        continue
+                    # ignore the cell if it is empty '0' or visited before '2'
+                    if grid[xx][yy] == 0 or grid[xx][yy] == 2:
+                        continue
+                
+                    fresh_cnt -= 1                    
+                    grid[xx][yy] = 2
+                    
+                    rotten.append((xx, yy))
+        return minutes_passed if fresh_cnt == 0 else -1
